de900e25b78e4dfe1a80d7756e535e6791e1fc98
Nora Itafti <50273078+noraa27@users.noreply.github.com>
2023-11-22 11:44:50 +0000

    add changes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Nora Itafti Rivas",
+  title: "Software Engineer",
+  email: "noraaa2706@gmail.com",
+  gitHub: "noraa27",
+  instagram: "micr27norairosoft",
+  linkedIn: "Nora Itafti Rivas",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
